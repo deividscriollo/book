0c48420bdc22354c7617498352ae40dec18af8bd
thyrons <willynarvaez6@gmail.com>
2015-09-11 15:17:22 -0500

    cambios

--- index/prueba.php
+++ index/prueba.php
@@ -1,45 +1,24 @@
-<?php	
-	function establecimientoSRI(){
-		$url='https://declaraciones.sri.gob.ec/facturacion-internet/consultas/publico/ruc-datos2.jspa';
-		$ch = curl_init();
+<html lang="en">
+  <head>
+    <meta name="google-signin-scope" content="profile email">
+    <meta name="google-signin-client_id" content="308434150280-e1djt4sf5ef7dlcol3imlbu0f765ncic.apps.googleusercontent.com">
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+  </head>
+  <body>
+    <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+    <script>
+      function onSignIn(googleUser) {
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log("Name: " + profile.getName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
 
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:15.0) Gecko/20100101 Firefox/15.0');
-		curl_setopt ($ch, CURLOPT_COOKIEJAR, dirname(__FILE__).'\cookie.txt');
-		curl_setopt($ch, CURLOPT_COOKIEFILE, dirname(__FILE__).'\cookie.txt');
-		curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-
-		$sri=curl_exec ($ch);
-
-		$lgnrnd=preg_replace('/^.*name="lgnrnd" value="/s','',$sri);
-		$lgnrnd=preg_replace('/".*$/s','',$lgnrnd);
-
-		$lgnjs=preg_replace('/^.*time=/s','',$sri);
-		$lgnjs=preg_replace('/&amp.*$/s','',$lgnjs);
-
-		$post = 'accion=siguiente&ruc=1090084247001';
-
-		curl_setopt($ch, CURLOPT_URL, 'https://declaraciones.sri.gob.ec/facturacion-internet/consultas/publico/ruc-datos2.jspa');
-		curl_setopt ($ch, CURLOPT_POST, 1);
-		curl_setopt ($ch, CURLOPT_POSTFIELDS, $post);
-		curl_exec ($ch);
-
-		
-		curl_setopt ($ch, CURLOPT_POST, 0);
-		curl_setopt($ch, CURLOPT_URL, 'https://declaraciones.sri.gob.ec/facturacion-internet/consultas/publico/ruc-establec.jspa');
-		curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);		
-		$res = curl_exec($ch);
-		curl_close($ch);		
-		
-		$filename = "cookie.txt";
-		$fa=fopen($filename, "w+");
-		fwrite($fa,"");
-		fclose($fa);
-		return $res;
-	}
-	print_r(establecimientoSRI());
-
-    
-?>
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+      };
+    </script>
+  </body>
+</html>
