651c6b393004a2a6a5f89ab5b8dc8d474d913903
thyrons <willynarvaez6@gmail.com>
2015-09-14 18:04:11 -0500

    cambios

--- admin/constante.php
+++ admin/constante.php
@@ -1,7 +1,13 @@
 <?php
-   	define("BD","book");
+   	/*define("BD","book");
 	define("SERVIDOR","localhost");
 	define("USUARIO","postgres");
 	define("CLAVE","rootdow");
+	define("PUERTO",5432);*/
+
+	define("BD","d9enpdt3pbr34t");
+	define("SERVIDOR","ec2-50-16-229-89.compute-1.amazonaws.com");
+	define("USUARIO","ojzzjzjvmcmdsk");
+	define("CLAVE","h9kpWbGB1D7NuUQlz8PSbsUuOX");
 	define("PUERTO",5432);
 ?>
