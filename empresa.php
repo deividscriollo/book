51b2e576e1f42bee4b343825e0b46ba6ac73b0db
Deivid <deividscriollo@gmail.com>
2015-11-17 10:49:58 -0500

    procesos

--- empresa.php
+++ empresa.php
@@ -7,6 +7,8 @@
     }
 	include_once('next/menu/app.php');
 	$classmenu=new menu();
+	$perfil=$_SESSION['m']['representante_legal'];
+    $nombre = explode(' ', $_SESSION['m']['representante_legal']);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -36,7 +38,7 @@
 		<!-- ace styles -->
 		<link rel="stylesheet" href="next/assets/css/ace.min.css" class="ace-main-stylesheet" id="main-ace-style" />
 		<link rel="stylesheet" href="next/assets/css/app.css" />
-		<link rel="stylesheet" href="next/dashboard/app.css" />
+		<link rel="stylesheet" href="next/empresa/app.css" />
 		<script src="next/assets/js/ace-extra.min.js"></script>
 	</head>
 
@@ -51,12 +53,99 @@
 			<div class="main-content">
 				<div class="main-content-inner">
 					<div class="page-content">
-						<div class="row no-padding">
+						<div class="row">
 							
 						</div>
 						<div class="row">
 							<div class="col-md-3">
-							hola m,undo
+								<div class="dd dd-draghandle">
+									<ol class="dd-list">
+										<li class="dd-item dd2-item" data-id="15">
+											<div class="dd-handle dd2-handle">
+												<i class="normal-icon ace-icon fa fa-signal orange bigger-130"></i>
+
+												<i class="drag-icon ace-icon fa fa-arrows bigger-125"></i>
+											</div>
+											<div class="dd2-content bg-origin"><?php print_r($_SESSION['m']['nom_comercial']); ?></div>
+
+											<ol class="dd-list">
+												<li class="dd-item dd2-item" data-id="16">
+													<div class="dd-handle dd2-handle">
+														<i class="normal-icon ace-icon fa fa-user red bigger-130"></i>
+
+														<i class="drag-icon ace-icon fa fa-arrows bigger-125"></i>
+													</div>
+													<div class="dd2-content">Galería de Eventos</div>
+												</li>
+
+												<li class="dd-item dd2-item dd-colored" data-id="17">
+													<div class="dd-handle dd2-handle btn-info">
+														<i class="normal-icon ace-icon fa fa-pencil-square-o bigger-130"></i>
+
+														<i class="drag-icon ace-icon fa fa-arrows bigger-125"></i>
+													</div>
+													<div class="dd2-content btn-info no-hover">Catalogo en línea</div>
+												</li>
+
+												<li class="dd-item dd2-item" data-id="18">
+													<div class="dd-handle dd2-handle">
+														<i class="normal-icon ace-icon fa fa-eye green bigger-130"></i>
+
+														<i class="drag-icon ace-icon fa fa-arrows bigger-125"></i>
+													</div>
+													<div class="dd2-content">Compras por Internet</div>
+												</li>
+												<li class="dd-item dd2-item" data-id="18">
+													<div class="dd-handle dd2-handle">
+														<i class="normal-icon ace-icon fa fa-eye green bigger-130"></i>
+
+														<i class="drag-icon ace-icon fa fa-arrows bigger-125"></i>
+													</div>
+													<div class="dd2-content">Trabaja con nosotros</div>
+												</li>
+											</ol>
+										</li>
+										<li class="dd-item dd2-item" data-id="15">
+												<div class="dd-handle dd2-handle">
+													<i class="normal-icon ace-icon fa fa-signal orange bigger-130"></i>
+
+													<i class="drag-icon ace-icon fa fa-arrows bigger-125"></i>
+												</div>
+												<div class="dd2-content bg-origin">Ofertas</div>
+
+												<ol class="dd-list">
+													<li class="dd-item dd2-item" data-id="16">
+														<div class="dd-handle dd2-handle">
+															<i class="normal-icon ace-icon fa fa-user red bigger-130"></i>
+
+															<i class="drag-icon ace-icon fa fa-arrows bigger-125"></i>
+														</div>
+														<div class="dd2-content">Active Users</div>
+													</li>
+
+													<li class="dd-item dd2-item dd-colored" data-id="17">
+														<div class="dd-handle dd2-handle btn-info">
+															<i class="normal-icon ace-icon fa fa-pencil-square-o bigger-130"></i>
+
+															<i class="drag-icon ace-icon fa fa-arrows bigger-125"></i>
+														</div>
+														<div class="dd2-content btn-info no-hover">Published Articles</div>
+													</li>
+
+													<li class="dd-item dd2-item" data-id="18">
+														<div class="dd-handle dd2-handle">
+															<i class="normal-icon ace-icon fa fa-eye green bigger-130"></i>
+
+															<i class="drag-icon ace-icon fa fa-arrows bigger-125"></i>
+														</div>
+														<div class="dd2-content">Visitors</div>
+													</li>
+												</ol>
+											</li>
+
+										
+									</ol>
+								</div>
 							</div>
 							<div class="col-md-9">
 								<div id="myCarousel" class="carousel slide" data-interval="3000" data-ride="carousel">
@@ -99,6 +188,9 @@
 								    </a>
 								</div>
 							</div>
+						</div>
+						<div class="row">
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.7443260775435!2d-78.12452172193899!3d0.34894804814544716!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e2a3cb8112674c7%3A0x534db44e277f860e!2sJaime+Rivadeneira%2C+Ibarra!5e0!3m2!1ses!2sec!4v1447773698416" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 						</div>
 					</div><!-- /.page-content -->
 				</div>
@@ -155,6 +247,7 @@
 		<script src="next/assets/js/jquery.validate.min.js"></script>
 		<script src="next/assets/js/jquery.blockUI.js"></script>
 		<script src="next/assets/js/sweetalert.min.js"></script>
+		<script src="next/assets/js/jquery.nestable.min.js"></script>
 		
 		
 
@@ -164,7 +257,19 @@
 
 		<!-- inline scripts related to this page -->
 		<!-- plugins media -->
-		<script src="next/dashboard/app.js"></script>
+		<script type="text/javascript">
+			jQuery(function($){
+			
+				$('.dd').nestable();
+			
+				$('.dd-handle a').on('mousedown', function(e){
+					e.stopPropagation();
+				});
+				
+				$('[data-rel="tooltip"]').tooltip();
+			
+			});
+		</script>
 	</body>
 </html>
 
--- next/admin/constante.php
+++ next/admin/constante.php
@@ -1,10 +1,10 @@
 <?php
 	// ------------------------ Informacion base de datos local------------------------------//
- 	// define("BD","book");
-	// define("SERVIDOR","localhost");
-	// define("USUARIO","postgres");
-	// define("CLAVE","rootdow");
-	// define("PUERTO",5432);
+ 	define("BD","book");
+	define("SERVIDOR","localhost");
+	define("USUARIO","postgres");
+	define("CLAVE","rootdow");
+	define("PUERTO",5432);
 
 	// ------------------------ Informacion conexion servidor dominio proceso correo------------------------------//
 	define("IPSERVER","67.205.125.28");
@@ -21,9 +21,9 @@
 	// define("PUERTO",5432);
 
 	// ------------------------ Informacion base de datos dominio ------------------------------//
-	define("BD","nextbook_book");
-	define("SERVIDOR","localhost");
-	define("USUARIO","nextbook_root");
-	define("CLAVE","WZ_aNTOCg-oX");
-	define("PUERTO",5432);
+	// define("BD","nextbook_book");
+	// define("SERVIDOR","localhost");
+	// define("USUARIO","nextbook_root");
+	// define("CLAVE","WZ_aNTOCg-oX");
+	// define("PUERTO",5432);
 ?>
