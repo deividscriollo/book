b5ed82e813369174f9ff6f9586db3e7c237940ab
Deivid <deividscriollo@gmail.com>
2015-12-08 17:41:52 -0500

    procesos cambiar estructura facturanext

--- facturanext.com/index.php
+++ facturanext.com/index.php
@@ -155,27 +155,10 @@
 			
 			<div class="main-content">
 				<div class="main-content-inner">
-					<div class="breadcrumbs" id="breadcrumbs">
-						<script type="text/javascript">
-							try{ace.settings.check('breadcrumbs' , 'fixed')}catch(e){}
-						</script>
-
-						<ul class="breadcrumb">
-							<li>
-								<i class="ace-icon fa fa-home home-icon"></i>
-								<a href="#">Inicio</a>
-							</li>
-
-							<li>
-								<a href="#">Facturas</a>
-							</li>							
-							<li class="active">Facturas Electrónicas</li>
-						</ul><!-- /.breadcrumb -->
-					</div>
 
 					<div class="page-content">
 						<div class="row">
-							<div class="tabbable">
+							<div class="tabbable tabs-left">
 								<ul class="nav nav-tabs" id="myTab">
 									<li class="active">
 										<a data-toggle="tab" href="#home">
